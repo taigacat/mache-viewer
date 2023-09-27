d8cce39d777fb1e128215821c5ae6495bd54203f
taigacat <taigacat@users.noreply.github.com>
2023-09-28 01:05:14 +0900

    Integrate logger into Lambda handler function
    A logger implementation from '@aws-lambda-powertools/logger' has been added to the handler function in 'backend/src/app.ts'. This ensures consistent logging across the application and makes it easier to trace and debug issues. The logger is initialized in a new 'logger.ts' file.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,8 +4,11 @@ import jsonBodyParser from '@middy/http-json-body-parser';
 import httpHeaderNormalizer from '@middy/http-header-normalizer';
 import httpRouterHandler from '@middy/http-router';
 import { routes } from './app-router';
+import { injectLambdaContext } from '@aws-lambda-powertools/logger';
+import { logger } from './logger';
 
 export const handler = middy()
+  .use(injectLambdaContext(logger))
   .use(httpHeaderNormalizer())
   .handler(httpRouterHandler(routes))
   .use(jsonBodyParser())
