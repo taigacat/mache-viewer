57c4fbc44d1313f19344235de3011590287dc7ba
taigacat <taigacat@users.noreply.github.com>
2023-10-07 20:05:50 +0900

    add GetStream handler

--- backend/src/handler/get-streams.handler.ts
+++ backend/src/handler/get-streams.handler.ts
@@ -1,11 +1,34 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { GetStreamListUsecase } from '../usecase/stream/get-stream-list.usecase';
 
 export const GetStreamsHandler: APIGatewayProxyHandler = async (event) => {
-  console.log(event);
+  const usecase = new GetStreamListUsecase();
+
+  if (!event.queryStringParameters) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'broadcasterId is required',
+      }),
+    };
+  }
+
+  const broadcasterId = event.queryStringParameters['broadcasterId'];
+  const nextToken = event.queryStringParameters['nextToken'];
+
+  if (!broadcasterId) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'broadcasterId is required',
+      }),
+    };
+  }
+
+  const result = await usecase.run({ broadcasterId, nextToken });
+
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello World!',
-    }),
+    body: JSON.stringify(result),
   };
 };
